1045aa6bebab19b80df571c163447658fdd66ecb
那可心 <nakx@raysdata.com>
2021-08-13 14:47:03 +0800

    初步实现

--- js/Draggable.js
+++ js/Draggable.js
@@ -19,6 +19,7 @@
     const $container = this.$container;
     const $el = this.$el;
     $container.style.position = 'relative';
+    $el.style.overflow = 'hidden';
     $el.style.position = 'absolute';
     this.containerWidth = $container.clientWidth;
     this.containerHeight = $container.clientHeight;
@@ -28,22 +29,120 @@
 
   Draggable.prototype.createOperator = function () {
     const $el = this.$el;
-    const charArray = ['t', 'r', 'b', 'l', 'tl', 'tr', 'rb', 'bl'];
+    const charArray = ['t', 'r', 'b', 'l', 'tl', 'tr', 'bl', 'br'];
     let operatorArray = [];
+
     charArray.forEach(item => {
       let $span = document.createElement('span');
       $span.className = item;
       $span.style.position = 'absolute';
+      $span.style.boxSizing = 'border-box';
       if (item.length === 1) {
         $span.style.background = 'transparent';
+        if (item === 't') {
+          $span.style.height = '10px';
+          $span.style.width = '100%';
+          $span.style.top = '0px';
+        }
+        if (item === 'r') {
+          $span.style.height = '100%';
+          $span.style.width = '10px';
+          $span.style.top = '0px';
+          $span.style.right = '0px';
+        }
+        if (item === 'b') {
+          $span.style.height = '10px';
+          $span.style.width = '100%';
+          $span.style.bottom = '0px';
+        }
+        if (item === 'l') {
+          $span.style.height = '100%';
+          $span.style.width = '10px';
+          $span.style.top = '0px';
+          $span.style.left = '0px';
+        }
       }
       if (item.length === 2) {
-        $span.style.width = '10px';
-        $span.style.height = '10px';
+        $span.style.width = '16px';
+        $span.style.height = '16px';
+        $span.style.border = '1px solid #ccc';
         $span.style.background = 'yellow';
+        if (item === 'tl') {
+          $span.style.top = '-5px';
+          $span.style.left = '-5px';
+        }
+        if (item === 'tr') {
+          $span.style.top = '-5px';
+          $span.style.right = '-5px';
+        }
+        if (item === 'bl') {
+          $span.style.bottom = '-5px';
+          $span.style.left = '-5px';
+        }
+        if (item === 'br') {
+          $span.style.bottom = '-5px';
+          $span.style.right = '-5px';
+        }
       }
+      $span.onmousedown = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        const $el = this.$el;
+        const $span = e.target;
+
+        let oldWidth = $el.offsetWidth;
+        let oldHeight = $el.offsetHeight;
+        let oldX = e.pageX;
+        let oldY = e.pageY;
+        let oldLeft = $el.offsetLeft;
+        let oldTop = $el.offsetTop;
+
+        root.onmousemove = e => {
+          if ($span.className == 'tl') {
+            $el.style.width = oldWidth - (e.pageX - oldX) + 'px';
+            $el.style.height = oldHeight - (e.pageY - oldY) + 'px';
+            $el.style.left = oldLeft + (e.pageX - oldX) + 'px';
+            $el.style.top = oldTop + (e.pageY - oldY) + 'px';
+          } else if ($span.className == 'bl') {
+            $el.style.width = oldWidth - (e.pageX - oldX) + 'px';
+            $el.style.height = oldHeight + (e.pageY - oldY) + 'px';
+            $el.style.left = oldLeft + (e.pageX - oldX) + 'px';
+            $el.style.bottom = oldTop + (e.pageY + oldY) + 'px';
+          } else if ($span.className == 'tr') {
+            $el.style.width = oldWidth + (e.pageX - oldX) + 'px';
+            $el.style.height = oldHeight - (e.pageY - oldY) + 'px';
+            $el.style.right = oldLeft - (e.pageX - oldX) + 'px';
+            $el.style.top = oldTop + (e.pageY - oldY) + 'px';
+          } else if ($span.className == 'br') {
+            $el.style.width = oldWidth + (e.pageX - oldX) + 'px';
+            $el.style.height = oldHeight + (e.pageY - oldY) + 'px';
+            $el.style.right = oldLeft - (e.pageX - oldX) + 'px';
+            $el.style.bottom = oldTop + (e.pageY + oldY) + 'px';
+          } else if ($span.className == 't') {
+            $el.style.height = oldHeight - (e.pageY - oldY) + 'px';
+            $el.style.top = oldTop + (e.pageY - oldY) + 'px';
+          } else if ($span.className == 'b') {
+            $el.style.height = oldHeight + (e.pageY - oldY) + 'px';
+            $el.style.bottom = oldTop - (e.pageY + oldY) + 'px';
+          } else if ($span.className == 'l') {
+            $el.style.height = oldHeight + 'px';
+            $el.style.width = oldWidth - (e.pageX - oldX) + 'px';
+            $el.style.left = oldLeft + (e.pageX - oldX) + 'px';
+          } else if ($span.className == 'r') {
+            $el.style.height = oldHeight + 'px';
+            $el.style.width = oldWidth + (e.pageX - oldX) + 'px';
+            $el.style.right = oldLeft - (e.pageX - oldX) + 'px';
+          }
+          this.handleOverBoundary();
+        };
+        root.onmouseup = e => {
+          root.onmousemove = null;
+          root.onmouseup = null;
+        };
+      };
       operatorArray.push($span);
     });
+
     operatorArray.forEach(item => {
       $el.appendChild(item);
     });
